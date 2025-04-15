9b78ad9dccaabc9ded544498d140bbbaa10a8c2d
Constantine Manakov <konstantinmanakow@gmail.com>
2025-04-15 17:33:16 +0200

    refactor: remove unnecessary trgm indexes and extension for timezones migration

--- priv/repo/migrations/20250415131729_timezones.exs
+++ priv/repo/migrations/20250415131729_timezones.exs
@@ -13,16 +13,11 @@ defmodule PhraseTask.Repo.Migrations.Timezones do
     end
 
     execute "create extension fuzzystrmatch;"
-    execute "create extension pg_trgm;"
     execute "create index timezones_title_dm on timezones using gin (daitch_mokotoff(title)) with (fastupdate = off);"
-    execute "create index timezones_title_trgm on timezones using gin (title gin_trgm_ops);"
-    execute "create index timezones_location_trgm on timezones using gin (pretty_timezone_location gin_trgm_ops);"
   end
 
   def down do
     execute "drop index timezones_title_dm;"
-    execute "drop index timezones_title_trgm;"
-    execute "drop index timezones_location_trgm;"
 
     drop table(:timezones)
   end
