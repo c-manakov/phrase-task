a9b823167a98e9ab3998d6ef1c04f2ff2eb5cef3
Constantine Manakov <konstantinmanakow@gmail.com>
2025-04-15 17:00:19 +0200

    feat: add migration for timezones table with trigram indexing

---
+++ priv/repo/migrations/20250415131729_timezones.exs
@@ -0,0 +1,24 @@
+defmodule PhraseTask.Repo.Migrations.Timezones do
+  use Ecto.Migration
+  
+  def up do
+    create table(:timezones) do
+      add :title, :string
+      add :timezone_id, :string
+      add :pretty_timezone_location, :string
+      add :timezone_abbr, :string
+      add :utc_to_dst_offset, :integer
+
+      timestamps()
+    end
+
+    execute "create extension pg_tgrm;"
+    execute "create index timezones_title_tgrm ON timezones USING gin (title gin_trgm_ops);"
+  end
+
+  def down do
+    execute "drop index timezones_title_tgrm;"
+
+    drop table(:timezones)
+  end
+end
