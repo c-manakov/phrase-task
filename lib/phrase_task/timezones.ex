c11510a4616b0d2e8278b4666d88b9851b0b4b7a
Constantine Manakov <konstantinmanakow@gmail.com>
2025-04-15 17:10:05 +0200

    refactor: improve timezone search with trigram similarity comments

--- lib/phrase_task/timezones.ex
+++ lib/phrase_task/timezones.ex
@@ -9,32 +9,37 @@ defmodule PhraseTask.Timezones do
 
   @doc """
   Returns a list of timezones that contain the given search string.
-  
+
   ## Examples
-  
+
       iex> search_timezones("New York")
       [%Timezone{}, ...]
-  
+
   """
   def search_timezones(search_string) when is_binary(search_string) and search_string != "" do
     # First try exact matching with ILIKE
-    exact_matches = 
+    exact_matches =
       from(t in Timezone,
-        where: ilike(t.title, ^"%#{search_string}%") or
-               ilike(t.pretty_timezone_location, ^"%#{search_string}%"),
+        where:
+          ilike(t.title, ^"%#{search_string}%") or
+            ilike(t.pretty_timezone_location, ^"%#{search_string}%"),
         order_by: t.title
       )
       |> Repo.all()
-    
+
     # If we have exact matches, return them
     if length(exact_matches) > 0 do
       exact_matches
     else
-      # Otherwise, use trigram similarity for fuzzy matching
-      # This helps with typos and misspellings
+      # this won't work need to insert raw query fragment like so SELECT t, word_similarity('word', t) AS sml
+      # FROM test_trgm
+      # WHERE 'word' % t
+      # ORDER BY sml DESC, t;
+# % is similarity, use this operator AI!
       from(t in Timezone,
-        where: fragment("? % ?", t.title, ^search_string) or
-               fragment("? % ?", t.pretty_timezone_location, ^search_string),
+        where:
+          fragment("? % ?", t.title, ^search_string) or
+            fragment("? % ?", t.pretty_timezone_location, ^search_string),
         order_by: [
           desc: fragment("similarity(?, ?)", t.title, ^search_string),
           desc: fragment("similarity(?, ?)", t.pretty_timezone_location, ^search_string),
@@ -45,17 +50,17 @@ defmodule PhraseTask.Timezones do
       |> Repo.all()
     end
   end
-  
+
   def search_timezones(_), do: []
 
   @doc """
   Returns the list of all timezones.
-  
+
   ## Examples
-  
+
       iex> list_timezones()
       [%Timezone{}, ...]
-  
+
   """
   def list_timezones do
     Repo.all(Timezone)
@@ -63,15 +68,15 @@ defmodule PhraseTask.Timezones do
 
   @doc """
   Gets a single timezone by ID.
-  
+
   ## Examples
-  
+
       iex> get_timezone(123)
       %Timezone{}
       
       iex> get_timezone(456)
       nil
-  
+
   """
   def get_timezone(id), do: Repo.get(Timezone, id)
 end
