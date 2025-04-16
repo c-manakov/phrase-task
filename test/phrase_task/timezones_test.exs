a1683eb663655015ed4496ba4097ca68328a06b7
Constantine Manakov <konstantinmanakow@gmail.com>
2025-04-16 16:42:27 +0200

    refactor: optimize timezone test setup with list insertion

--- test/phrase_task/timezones_test.exs
+++ test/phrase_task/timezones_test.exs
@@ -6,7 +6,7 @@ defmodule PhraseTask.TimezonesTest do
 
   describe "search_timezones/1" do
     setup do
-      # Insert some test timezones
+      # replace this with a list and insert the list all at once AI!
       {:ok, _} = %Timezone{title: "America/New_York", timezone_id: "america_new_york", pretty_timezone_location: "New York", timezone_abbr: "EST", utc_to_dst_offset: -18000} |> Repo.insert()
       {:ok, _} = %Timezone{title: "Europe/London", timezone_id: "europe_london", pretty_timezone_location: "London", timezone_abbr: "GMT", utc_to_dst_offset: 0} |> Repo.insert()
       {:ok, _} = %Timezone{title: "Asia/Tokyo", timezone_id: "asia_tokyo", pretty_timezone_location: "Tokyo", timezone_abbr: "JST", utc_to_dst_offset: 32400} |> Repo.insert()
